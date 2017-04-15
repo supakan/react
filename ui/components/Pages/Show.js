784cfaf61fc747f29465b6f55f8e53a5a0cadd83
Supakan Supapinyopong <supakan.s@cubesofttech.com>
2017-04-15 17:38:28 +0700

    End Day 2 npm i --save-dev json-server For JSON Server npm i --save-dev npm-run-all For Create Muti Run For Package.JSON npm install --save react-router For Create Link npm install --save-dev babel-preset-stage-0 For Open Index When Start npm i --save isomorphic-fetch Ajax In React

---
+++ ui/components/Pages/Show.js
@@ -0,0 +1,23 @@
+// ui/components/Pages/Show.js
+import React, { PropTypes } from 'react'
+
+const ShowPage = ({
+  title,
+  content
+}) => {
+  return (
+    <article>
+      <h1>{title}</h1>
+      <p>
+        {content}
+      </p>
+    </article>
+  )
+}
+
+ShowPage.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired
+}
+
+export default ShowPage
--- webpack.config.js
+++ webpack.config.js
@@ -66,9 +66,13 @@
 const webpack = require('webpack');
 const path = require('path');
 const autoprefixer = require('autoprefixer');
+
 module.exports = {
   devtool: 'eval',
-  entry: './src/index.js',
+  entry: [
+    './ui/theme/elements.scss',
+    './ui/index.js'
+  ],
   output: {
     publicPath: '/static/',
     path: path.join(__dirname, 'static'),
@@ -76,25 +80,23 @@ module.exports = {
   },
   module: {
     loaders: [
-  {
-    test: /\.jsx?$/,
-    exclude: /node_modules/,
-    loaders: [
-      'babel-loader'
-    ]
-  },
-  {
-    // สำหรับไฟล์นามสกุล css ให้ใช้ Loader สองตัวคือ css-loader และ style-loader
-    test: /\.css$/,
-    loaders: [
-      'style-loader',
-      'css-loader'
-    ]
-  }, {
-    // ใช้ Loader สามตัวสำหรับ scss
-    test: /\.scss$/,
-    exclude: /node_modules/,
-    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: [
           'style-loader',
           {
             loader: 'css-loader',
@@ -111,9 +113,19 @@ module.exports = {
               sourceMap: true
             }
           },
-          'postcss-loader' // เพิ่ม postcss
+          {
+             loader: 'postcss-loader'
+           }
+        ]
+      }
     ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      '/api/*': {
+        target: 'http://127.0.0.1:5000'
+      }
+    }
   }
-  ]
-}
 };
